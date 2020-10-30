21e855ed861b6bbcc0c146b28fd5eb7369086aea
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-10-30 15:07:27 +0100

    Refactoring of lifting state

--- 03_Thursday/day3_exercise/src/App.js
+++ 03_Thursday/day3_exercise/src/App.js
@@ -1,11 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
+import StateDemo from './LiftingUp';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <StateDemo />
     </div>
   );
 }
